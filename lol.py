e1672b01d7270d4819347a19c9db7bc4df2b2bb5
richthofen80 <richthofen80@localhost.localdomain>
2015-03-31 16:38:15 -0400

    sid

--- lol.py
+++ lol.py
@@ -112,8 +112,8 @@ def showPlayerScores(statsList):
 #        print score(stat[0], stat[1], stat[2], stat[3], stat[4], stat[5], stat[6])
         return score(stat[0], stat[1], stat[2], stat[3], stat[4], stat[5], stat[6])
 
-
-players = getCurrentGamePlayers(43310476)
+mySummonerId = 43310476
+players = getCurrentGamePlayers(mySummonerId)
 team1 = []
 team2 = []
 for player in players:
